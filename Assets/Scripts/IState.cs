258ee96cab5f05c2dd02e11a08ecfdebcaa3bdcc
Henrique Szabo <henrique.szabo@tappsvr.com>
2023-03-14 09:08:49 -0300

    Feat: Add State Machine Pattern Base

---
+++ Assets/Scripts/IState.cs
@@ -0,0 +1,9 @@
+﻿namespace RickSzaboDev.Patterns
+{
+    public interface IState
+    {
+        void Tick();
+        void OnEnter();
+        void OnExit();
+    }
+}
---
+++ Assets/Scripts/UI/LookAtCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace RickSzaboDev.Behaviour
+{
+    public class LookAtCamera : MonoBehaviour
+    {
+        [SerializeField] private Transform CameraTransform;
+
+        private void Update()
+        {
+            if (!CameraTransform)
+            {
+                Debug.LogWarning($"Trying to make {gameObject.name} look at camera, but no camera target was assigned.");
+                return;
+            }
+
+            transform.rotation = Quaternion.LookRotation(transform.position - CameraTransform.position);
+        }
+    }
+}
